9b71aabe76636a7ec10b238cc287c8fa344a1637
eslamaboudesheesh <eslamaboudesheesh@hotmail.com>
2023-08-11 07:44:59 +0300

    feat(add animation ) : add animation to list product page

--- src/app/shared/animations/fade.animation.ts
+++ src/app/shared/animations/fade.animation.ts
@@ -10,6 +10,6 @@ import {
 export const Fade = trigger('fade', [
   state('visible', style({ opacity: 1 })),
   state('void,hidden', style({ opacity: 0 })),
-  transition('*=>visible', animate('1000ms ease-out')),
-  transition('*=>void,*=>hidden', animate('400ms ease-out')),
+  transition('*=>visible', animate('2000ms ease-out')),
+  transition('*=>void,*=>hidden', animate('900ms ease-out')),
 ]);
