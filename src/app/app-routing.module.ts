0538571b525baa631a1e13d510799699635b44ad
eslamaboudesheesh <eslamaboudesheesh@hotmail.com>
2023-08-11 07:29:52 +0300

    fix( route  ) : change route direction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   {
     path: 'Admin',
     component: ContentLayoutComponent,
-    canActivate: [authGuardIsAdmin],
+    canActivateChild: [authGuardIsAdmin],
 
     children: [
       {
@@ -49,6 +49,7 @@ const routes: Routes = [
       },
     ],
   },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
